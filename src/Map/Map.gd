6410cdd3a68f505350ff09064fa5db9684e9ac2c
kissesy <39688948+kissesy@users.noreply.github.com>
2021-07-11 23:03:33 +0900

    상품 마우스 조작 가능

--- src/Map/Map.gd
+++ src/Map/Map.gd
@@ -4,8 +4,9 @@ extends Node2D
 onready var Clock = $Clock
 onready var Background = $Background
 
-var state
 
+
+var held_object = null
 var time_gap = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -21,7 +22,29 @@ func setup():
 	add_child(clock_timer)
 	clock_timer.start()
 	
+	load_product()
+	load_product()
 	
 func _on_clock_timeout():
 	State.set_time(time_gap)
 	Clock.get_node("Label").text = State.get_time()
+
+
+
+func load_product():
+	var cola_instance = preload("res://src/Product/Cola.tscn").instance()
+	cola_instance.connect("clicked", self, "_on_product_pickable_clicked")
+	cola_instance
+	get_node("InStore/Storage").add_child(cola_instance)
+	cola_instance.position = $InStore/Delivery.position
+
+func _on_product_pickable_clicked(object):
+	if !held_object:
+		held_object = object
+		held_object.pickup()
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if held_object and !event.pressed:
+			held_object.drop(Input.get_last_mouse_speed())
+			held_object = null
--- src/Product/Object.gd
+++ src/Product/Object.gd
@@ -1,12 +1,45 @@
 extends RigidBody2D
 
-
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
 
+const GRAVITY = 9.8
+
+
+var dragging = false 
+var held = false
+
+signal clicked
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	if held:
+		global_transform.origin = get_global_mouse_position()
+
+
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			emit_signal("clicked", self)
+					
+func pickup():
+	if held:
+		return
+	mode = RigidBody2D.MODE_STATIC
+	held = true
+
+func drop(impulse=Vector2.ZERO):
+	if held:
+		mode = RigidBody2D.MODE_RIGID
+		apply_central_impulse(impulse)
+		held = false
+
+
+func _on_VisibilityNotifier2D_viewport_exited(viewport: Viewport) -> void:
+	print("free~!")
+	queue_free()
